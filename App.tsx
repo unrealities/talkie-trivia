97ae016a2254d02b3066859bb52e48708636297a
Tom Szymanski <szymanski.tom@gmail.com>
2022-05-28 11:20:11 -0500

    trouble with secrets

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react';
+import Constants from 'expo-constants';
 
 export default function App() {
+  useEffect(() => {
+    getMoviesFromApi(5)
+  
+    function getMoviesFromApi(id:number) {
+      const movieURL = new URL('https://example.com')
+      movieURL.protocol = 'https'
+      movieURL.hostname = 'api.themoviedb.org'
+      movieURL.pathname = `/3/movie/${id}`
+      movieURL.search = `api_key=${Constants.manifest.extra.themoviedb_key}`
+
+      fetch(movieURL.href)
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log('getting data from fetch', responseJson)
+            })
+            .catch(error => console.log(error))
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Check the console</Text>
       <StatusBar style="auto" />
     </View>
   );
--- data/fetch.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-let movieID = 5
-const movieURL = new URL('https://example.com')
-movieURL.protocol = 'https'
-movieURL.hostname = 'api.themoviedb.org'
-movieURL.pathname = `/3/movie/${movieID}`
-movieURL.search = `api_key=${process.env.THEMOVIEDB_APIKEY}`
-movieURL
