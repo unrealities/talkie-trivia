fe1c94ab2730095dd4f7b3ec9210b7feee014099
Tom Szymanski <szymanski.tom@gmail.com>
2022-06-01 11:34:25 -0500

    async fun

--- App.tsx
+++ App.tsx
@@ -2,38 +2,30 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 import Constants from 'expo-constants'
-import fs from 'browserify-fs'
+import PopularMovies from './data/popular_movies_05_24_2022.json'
+
 
 export default function App() {
   useEffect(() => {
-    // let movies = require('./data/popular_movies_05_24_2022.json');
-    // for (var id in movies.id) {
-    //   getMoviesFromApi(id)
-    // }
-    
-    getMoviesFromApi('5')
-
-    function getMoviesFromApi(id:string) {
+    const fetchData = async () => {
       const movieURL = new URL('https://example.com')
       movieURL.protocol = 'https'
       movieURL.hostname = 'api.themoviedb.org'
-      movieURL.pathname = `/3/movie/${id}`
       movieURL.search = `api_key=${Constants.manifest.extra.themoviedb_key}`
-
-      fetch(movieURL.href)
-            .then(response => response.json())
-            .then((responseJson) => {
-              const data = JSON.stringify(responseJson)
-              fs.writeFile('detailed_movies.json', data, (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log("JSON data is saved.");
-            });
-            })
-            .catch(error => console.log(error))
+      let data = '['
+      
+      // for (const movie of PopularMovies) {
+      //   movieURL.pathname = `/3/movie/${movie.id}`
+      //   let response = await fetch(movieURL.href)
+      //   data += JSON.stringify(response.json()) + ","
+      // }
+      data += ']'
+      return data
     }
-  });
+
+    let result = fetchData()
+    console.log(result)
+  })
 
   return (
     <View style={styles.container}>
