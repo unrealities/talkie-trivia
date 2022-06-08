45c4f315b4f57ad99cff4c18d878a42acee93865
Tom Szymanski <szymanski.tom@gmail.com>
2022-06-08 10:04:56 -0500

    start making a movie model

--- App.tsx
+++ App.tsx
@@ -1,32 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import Constants from 'expo-constants'
-import PopularMovies from './data/popular_movies_05_24_2022.json'
-
+import React from 'react'
 
 export default function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const movieURL = new URL('https://example.com')
-      movieURL.protocol = 'https'
-      movieURL.hostname = 'api.themoviedb.org'
-      movieURL.search = `api_key=${Constants.manifest.extra.themoviedb_key}`
-      let data = '['
-      
-      // for (const movie of PopularMovies) {
-      //   movieURL.pathname = `/3/movie/${movie.id}`
-      //   let response = await fetch(movieURL.href)
-      //   data += JSON.stringify(response.json()) + ","
-      // }
-      data += ']'
-      return data
-    }
-
-    let result = fetchData()
-    console.log(result)
-  })
-
   return (
     <View style={styles.container}>
       <Text>Check the console</Text>
