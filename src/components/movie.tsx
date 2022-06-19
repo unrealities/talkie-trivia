285dcca36a061af879b0474dea9bf7288604d7f2
Tom Szymanski <szymanski.tom@gmail.com>
2022-06-19 18:31:38 -0400

    figure out poster path

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -69,6 +69,7 @@ export interface Movie {
           <Text>IMDB Link: https://www.imdb.com/title/{movie.imdb_id}/</Text>
         </TouchableOpacity>
         <Text>{movie.overview}</Text>
+        <Text>{movie.poster_path}</Text>
       </View>
     )
   }
