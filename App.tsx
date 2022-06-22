614600064930e4e1610851aad500ba292da7f440
Tom Szymanski <szymanski.tom@gmail.com>
2022-06-22 10:35:19 -0400

    fix merge conflicts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import MoviesContainer from './src/components/movie'
+import Constants from 'expo-constants';
 
 export default function App() {
+  useEffect(() => {
+    getMoviesFromApi(5)
+  
+    function getMoviesFromApi(id:number) {
+      const movieURL = new URL('https://example.com')
+      movieURL.protocol = 'https'
+      movieURL.hostname = 'api.themoviedb.org'
+      movieURL.pathname = `/3/movie/${id}`
+      movieURL.search = `api_key=${Constants.manifest.extra.themoviedb_key}`
+
+      fetch(movieURL.href)
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log('getting data from fetch', responseJson)
+            })
+            .catch(error => console.log(error))
+    }
+  });
+
   return (
     <View style={styles.container}>
       <MoviesContainer />
