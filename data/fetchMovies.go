2a11dfbef353edba545674c3d5d165c223d7f015
Tom Szymanski <szymanski.tom@gmail.com>
2022-07-03 10:32:25 -0500

    fetch credits

--- data/fetchMovies.go
+++ data/fetchMovies.go
@@ -22,7 +22,7 @@ type Config struct {
 	TMDBKey string `json:"TMDBKey"`
 }
 
-type TMDBResponse struct {
+type TMDBDetailsResponse struct {
 	Adult               bool        `json:"adult"`
 	BackdropPath        string      `json:"backdrop_path"`
 	BelongsToCollection interface{} `json:"belongs_to_collection"`
@@ -65,6 +65,37 @@ type TMDBResponse struct {
 	VoteCount   int     `json:"vote_count"`
 }
 
+type TMDBCreditsResponse struct {
+	ID   int `json:"id"`
+	Cast []struct {
+		Adult              bool    `json:"adult"`
+		Gender             int     `json:"gender"`
+		ID                 int     `json:"id"`
+		KnownForDepartment string  `json:"known_for_department"`
+		Name               string  `json:"name"`
+		OriginalName       string  `json:"original_name"`
+		Popularity         float64 `json:"popularity"`
+		ProfilePath        string  `json:"profile_path"`
+		CastID             int     `json:"cast_id"`
+		Character          string  `json:"character"`
+		CreditID           string  `json:"credit_id"`
+		Order              int     `json:"order"`
+	} `json:"cast"`
+	Crew []struct {
+		Adult              bool    `json:"adult"`
+		Gender             int     `json:"gender"`
+		ID                 int     `json:"id"`
+		KnownForDepartment string  `json:"known_for_department"`
+		Name               string  `json:"name"`
+		OriginalName       string  `json:"original_name"`
+		Popularity         float64 `json:"popularity"`
+		ProfilePath        string  `json:"profile_path"`
+		CreditID           string  `json:"credit_id"`
+		Department         string  `json:"department"`
+		Job                string  `json:"job"`
+	} `json:"crew"`
+}
+
 func TMDBKey() string {
 	configFile, err := os.Open("secrets.json")
 	if err != nil {
@@ -92,7 +123,7 @@ func URLS() []string {
 	urls := make([]string, len(movies))
 
 	for _, movie := range movies {
-		path := "/3/movie/" + strconv.Itoa(movie.ID)
+		path := "/3/movie/" + strconv.Itoa(movie.ID) + "/credits"
 		url := url.URL{
 			Scheme: "https",
 			Host:   "api.themoviedb.org",
@@ -115,7 +146,7 @@ func WriteToFile(file *os.File, data string) {
 }
 
 func main() {
-	fileName := "data.txt"
+	fileName := "credits.txt"
 	urls := URLS()
 	f, err := os.Create(fileName)
 	if err != nil {
@@ -138,13 +169,15 @@ func main() {
 
 		// check for error response
 		data := (*json.RawMessage)(&body)
-		var validResponse TMDBResponse
+		// var validResponse TMDBDetailsResponse
+		var validResponse TMDBCreditsResponse
 		err = json.Unmarshal(*data, &validResponse)
 		if err != nil {
 			fmt.Printf(err.Error())
 		}
 
-		if validResponse.OriginalTitle > "" {
+		// if validResponse.OriginalTitle > "" {
+		if validResponse.ID > 0 {
 			WriteToFile(f, string(body))
 		}
 	}
