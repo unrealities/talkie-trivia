8cd76b146be5eebe737fc1e1cf06bfa4de84b9e9
Tom Szymanski <szymanski.tom@gmail.com>
2022-07-04 08:49:59 -0500

    smaller credits

--- data/fetchMoviesCredits.go
+++ data/fetchMoviesCredits.go
@@ -22,47 +22,35 @@ type Config struct {
 	TMDBKey string `json:"TMDBKey"`
 }
 
-type TMDBDetailsResponse struct {
-	Adult               bool        `json:"adult"`
-	BackdropPath        string      `json:"backdrop_path"`
-	BelongsToCollection interface{} `json:"belongs_to_collection"`
-	Budget              int         `json:"budget"`
-	Genres              []struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"genres"`
-	Homepage            string  `json:"homepage"`
-	ID                  int     `json:"id"`
-	ImdbID              string  `json:"imdb_id"`
-	OriginalLanguage    string  `json:"original_language"`
-	OriginalTitle       string  `json:"original_title"`
-	Overview            string  `json:"overview"`
-	Popularity          float64 `json:"popularity"`
-	PosterPath          string  `json:"poster_path"`
-	ProductionCompanies []struct {
-		ID            int    `json:"id"`
-		LogoPath      string `json:"logo_path"`
-		Name          string `json:"name"`
-		OriginCountry string `json:"origin_country"`
-	} `json:"production_companies"`
-	ProductionCountries []struct {
-		Iso31661 string `json:"iso_3166_1"`
-		Name     string `json:"name"`
-	} `json:"production_countries"`
-	ReleaseDate     string `json:"release_date"`
-	Revenue         int    `json:"revenue"`
-	Runtime         int    `json:"runtime"`
-	SpokenLanguages []struct {
-		EnglishName string `json:"english_name"`
-		Iso6391     string `json:"iso_639_1"`
-		Name        string `json:"name"`
-	} `json:"spoken_languages"`
-	Status      string  `json:"status"`
-	Tagline     string  `json:"tagline"`
-	Title       string  `json:"title"`
-	Video       bool    `json:"video"`
-	VoteAverage float64 `json:"vote_average"`
-	VoteCount   int     `json:"vote_count"`
+type TMDBCreditsResponse struct {
+	ID   int `json:"id"`
+	Cast []struct {
+		Adult              bool    `json:"adult"`
+		Gender             int     `json:"gender"`
+		ID                 int     `json:"id"`
+		KnownForDepartment string  `json:"known_for_department"`
+		Name               string  `json:"name"`
+		OriginalName       string  `json:"original_name"`
+		Popularity         float64 `json:"popularity"`
+		ProfilePath        string  `json:"profile_path"`
+		CastID             int     `json:"cast_id"`
+		Character          string  `json:"character"`
+		CreditID           string  `json:"credit_id"`
+		Order              int     `json:"order"`
+	} `json:"cast"`
+	Crew []struct {
+		Adult              bool    `json:"adult"`
+		Gender             int     `json:"gender"`
+		ID                 int     `json:"id"`
+		KnownForDepartment string  `json:"known_for_department"`
+		Name               string  `json:"name"`
+		OriginalName       string  `json:"original_name"`
+		Popularity         float64 `json:"popularity"`
+		ProfilePath        string  `json:"profile_path"`
+		CreditID           string  `json:"credit_id"`
+		Department         string  `json:"department"`
+		Job                string  `json:"job"`
+	} `json:"crew"`
 }
 
 func TMDBKey() string {
@@ -79,7 +67,7 @@ func TMDBKey() string {
 }
 
 func URLS() []string {
-	moviesFile, err := os.Open("popular_movies_05_24_2022.json")
+	moviesFile, err := os.Open("movies.json")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -92,7 +80,7 @@ func URLS() []string {
 	urls := make([]string, len(movies))
 
 	for _, movie := range movies {
-		path := "/3/movie/" + strconv.Itoa(movie.ID)
+		path := "/3/movie/" + strconv.Itoa(movie.ID) + "/credits"
 		url := url.URL{
 			Scheme: "https",
 			Host:   "api.themoviedb.org",
@@ -115,7 +103,7 @@ func WriteToFile(file *os.File, data string) {
 }
 
 func main() {
-	fileName := "movies.txt"
+	fileName := "credits.txt"
 	urls := URLS()
 	f, err := os.Create(fileName)
 	if err != nil {
@@ -138,13 +126,13 @@ func main() {
 
 		// check for error response
 		data := (*json.RawMessage)(&body)
-		var validResponse TMDBDetailsResponse
+		var validResponse TMDBCreditsResponse
 		err = json.Unmarshal(*data, &validResponse)
 		if err != nil {
 			fmt.Printf(err.Error())
 		}
 
-		if validResponse.OriginalTitle > "" {
+		if validResponse.ID > 0 {
 			WriteToFile(f, string(body))
 		}
 	}
