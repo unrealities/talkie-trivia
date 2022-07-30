3d78e757a22537c15c282005ad340399cbb10ceb
Tom Szymanski <szymanski.tom@gmail.com>
2022-07-30 08:13:48 -0500

    add a font

--- src/components/titleHeader.tsx
+++ src/components/titleHeader.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import AppLoading from 'expo-app-loading';
+import { useFonts, Arvo_400Regular } from '@expo-google-fonts/arvo'
 
 const TitleHeader = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Talkie-Trivia</Text>
-        </View>
-    )
+    let [fontsLoaded] = useFonts({
+        Arvo_400Regular,
+    })
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>TALKIE-TRIVIA</Text>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 1
     },
+    text: {
+        color: 'red',
+        fontFamily: 'Arvo_400Regular',
+        fontSize: 24,
+    }
 });
 
 export default TitleHeader
