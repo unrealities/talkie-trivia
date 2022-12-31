3ff674e7f78d90545797e3946d160db458e118e9
Tom Szymanski <szymanski.tom@gmail.com>
2022-12-31 08:34:00 -0600

    switch up colors

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,6 +1,8 @@
 export const colors = {
-    background: 'navy',
-    primary: 'orange',
-    secondary: 'white',
-    tertiary: 'black'
+    // https://coolors.co/palette/003049-d62828-f77f00-fcbf49-eae2b7
+    background: '#003049',
+    primary: '#f77f00',
+    secondary: '#eae2b7',
+    tertiary: '#fcbf49',
+    quaternary: '#d62828'
 }
