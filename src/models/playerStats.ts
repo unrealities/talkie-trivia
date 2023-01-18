24ba34c94ad1554445c94c4aa6f8f2d2adee8761
Tom Szymanski <szymanski.tom@gmail.com>
2023-01-18 08:26:20 -0600

    playerStats

---
+++ src/models/playerStats.ts
@@ -0,0 +1,12 @@
+export default class PlayerStats {
+    currentStreak: number
+    games: number
+    maxStreak: number
+    win1: number
+    win2: number
+    win3: number
+    win4: number
+    win5: number
+    wins: number
+    winPercentage: number
+}
