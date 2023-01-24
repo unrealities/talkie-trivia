b485d93dfb86567ff7841b7db139c50693332447
Tom Szymanski <szymanski.tom@gmail.com>
2023-01-24 08:47:17 -0600

    prepare to show playerStats

--- src/components/playerStats.tsx
+++ src/components/playerStats.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 import Player from '../models/player'
+import PlayerStats from '../models/playerStats'
 
 //TODO: This will display player lifetime statistics 
 //      either after completing a gain or from a profile menu
@@ -8,8 +10,26 @@ import Player from '../models/player'
 
 export interface PlayerStatsContainerProps {
     player: Player
+    playerStats: PlayerStats
 }
 
 const PlayerStatsContainer = (props: PlayerStatsContainerProps) => {
-
+    return (
+        <View style={styles.container}>
+            <Text>{props.playerStats.games}</Text>
+            <Text>{props.playerStats.currentStreak}</Text>
+            <Text>{props.playerStats.maxStreak}</Text>
+            <Text>{props.playerStats.wins[0]}</Text>
+            <Text>{props.playerStats.wins[1]}</Text>
+            <Text>{props.playerStats.wins[2]}</Text>
+            <Text>{props.playerStats.wins[3]}</Text>
+            <Text>{props.playerStats.wins[4]}</Text>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
