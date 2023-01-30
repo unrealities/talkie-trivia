91b93f12abd736bffbe7cf157f33ca5f838fac52
Tom Szymanski <szymanski.tom@gmail.com>
2023-01-30 08:57:50 -0600

    cannot figure out sizing. super small chart

--- src/components/winChart.tsx
+++ src/components/winChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Dimensions } from 'react-native'
+import { View, Dimensions, StyleSheet } from 'react-native'
 import { VictoryPie } from 'victory'
 import { colors } from '../styles/global'
 
@@ -16,7 +16,8 @@ const WinChart = (props: WinChartProps) => {
     console.log(props.wins)
 
     const keys = ['1', '2', '3', '4', '5']
-    const values = [props.wins[0], props.wins[1], props.wins[2], props.wins[3], props.wins[4]]
+    // const values = [props.wins[0], props.wins[1], props.wins[2], props.wins[3], props.wins[4]]
+    const values = [1,3,4,3,7]
     const data = keys.map((key, index) => {
         return {
             key: key,
@@ -27,12 +28,21 @@ const WinChart = (props: WinChartProps) => {
     const deviceWidth = Dimensions.get('window').width
 
     return (
-        <View style={{ justifyContent: 'center', flex: 1 }}>
+        <View style={styles.container}>
             <VictoryPie
                 colorScale={[colors.primary, colors.secondary, colors.tertiary, colors.quaternary, colors.quinary]}
-                data={data} />
+                data={data}
+                style={{data: {width: 300}}}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 8
+    }
+})
 
 export default WinChart
