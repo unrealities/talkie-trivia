138bdfa109100ef5a584c8176564cd879eaba212
Tom Szymanski <szymanski.tom@gmail.com>
2023-02-19 10:59:24 -0600

    environment variable trouble

--- app.config.js
+++ app.config.js
@@ -3,16 +3,14 @@ const myValue = 'talkie-trivia'
 module.exports = {
   name: myValue,
   version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
-  expo: {
-    extra: {
-      firebaseApiKey: process.env.FIREBASE_APIKEY,
-      firebaseAppId: process.env.FIREBASE_APPID,
-      firebaseAppId: process.env.FIREBASE_MEASUREMENTID,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-      firebaseProjectId: process.env.FIREBASE_PROJECTID,
-      themoviedb_key: process.env.THEMOVIEDB_APIKEY,
-      expoClientId: process.env.CLIENTID_EXPO,
-      webClientId: process.env.CLIENTID_WEB
-    }
+  extra: {
+    firebaseApiKey: process.env.FIREBASE_APIKEY,
+    firebaseAppId: process.env.FIREBASE_APPID,
+    firebaseAppId: process.env.FIREBASE_MEASUREMENTID,
+    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+    firebaseProjectId: process.env.FIREBASE_PROJECTID,
+    themoviedb_key: process.env.THEMOVIEDB_APIKEY,
+    expoClientId: process.env.CLIENTID_EXPO,
+    webClientId: process.env.CLIENTID_WEB
   }
 }
