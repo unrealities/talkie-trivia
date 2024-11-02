ee3417debdf9f6e6126ced23092d2e2d8214d16c
Tom Szymanski <szymanski.tom@gmail.com>
2024-11-01 20:44:43 -0500

    expo-doctor fixes

--- app.config.js
+++ app.config.js
@@ -1,23 +1,54 @@
 require("dotenv").config();
 
-const myValue = 'talkie-trivia'
+const myValue = 'talkie-trivia';
 
-export default {
-  name: myValue,
-  version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
-  extra: {
-    firebaseApiKey: process.env.FIREBASE_APIKEY,
-    firebaseAppId: process.env.FIREBASE_APPID,
-    firebaseAppId: process.env.FIREBASE_MEASUREMENTID,
-    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
-    firebaseProjectId: process.env.FIREBASE_PROJECTID,
-    themoviedb_key: process.env.THEMOVIEDB_APIKEY,
-    expoClientId: process.env.CLIENTID_EXPO,
-    iosClientId: process.env.CLIENTID_IOS,
-    webClientId: process.env.CLIENTID_WEB
-  },
-  plugins: [
-    "expo-font",
-    "expo-secure-store"
-  ]
-}
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    name: myValue,
+    slug: "talkie-trivia",
+    version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ["**/*"],
+    plugins: [
+      "@react-native-google-signin/google-signin",
+      "expo-font",
+      "expo-secure-store"
+    ],
+    ios: {
+      googleServicesFile: "./GoogleService-Info.plist",
+      supportsTablet: true
+    },
+    android: {
+      googleServicesFile: "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_APIKEY,
+      firebaseAppId: process.env.FIREBASE_APPID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENTID,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+      firebaseProjectId: process.env.FIREBASE_PROJECTID,
+      themoviedb_key: process.env.THEMOVIEDB_APIKEY,
+      expoClientId: process.env.CLIENTID_EXPO,
+      iosClientId: process.env.CLIENTID_IOS,
+      webClientId: process.env.CLIENTID_WEB
+    }
+  }
+});
