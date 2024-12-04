561016d356e2a19cf538c8976208032ecb2bba00
Tom Szymanski <szymanski.tom@gmail.com>
2024-12-04 10:19:51 -0600

    update winChart

--- src/components/winChart.tsx
+++ src/components/winChart.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { View } from "react-native"
-import Victory from "../utils/victory/victory"
 
+import Victory from "../utils/victory/victory"
 import { colors } from "../styles/global"
 import { winChartStyles } from "../styles/winChartStyles"
 
@@ -13,16 +13,15 @@ export interface WinChartProps {
 }
 
 const WinChart = (props: WinChartProps) => {
-  const keys = ["1", "2", "3", "4", "5"]
-  // const values = [props.wins[0], props.wins[1], props.wins[2], props.wins[3], props.wins[4]]
-  const values = [1, 3, 4, 3, 7]
-  const data = keys.map((key, index) => {
-    return {
-      key: key,
-      x: key,
-      y: values[index],
-    }
-  })
+  const keys = ["1", "2", "3", "4", "5"];
+  // Commented out original values line, using hardcoded for demonstration
+  const values = [1, 3, 4, 3, 7];
+  
+  const data = keys.map((key, index) => ({
+    key: key,
+    x: key,
+    y: values[index],
+  }))
 
   return (
     <View style={winChartStyles.container}>
