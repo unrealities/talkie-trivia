bd9b00c1dd38ffb2efcf92d52e9410a542e4087b
Tom Szymanski <szymanski.tom@gmail.com>
2024-12-10 10:14:59 -0600

    missed facts

--- src/styles/factsStyles.ts
+++ src/styles/factsStyles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native"
-import { colors, responsive } from "./global"
+import { responsive } from "./global"
 
 export const factsStyles = StyleSheet.create({
   container: {
@@ -7,10 +7,10 @@ export const factsStyles = StyleSheet.create({
     flex: 1,
     paddingBottom: responsive.scale(20),
     paddingHorizontal: responsive.scale(10),
-    width: responsive.scale(260),
+    width: responsive.isLargeScreen() ? "60%" : "90%",
+    maxWidth: responsive.scale(500),
   },
   header: {
-    flexWrap: "wrap",
     fontFamily: "Arvo-Bold",
     fontSize: responsive.responsiveFontSize(24),
     paddingBottom: responsive.scale(10),
@@ -18,22 +18,18 @@ export const factsStyles = StyleSheet.create({
     color: "#333",
   },
   scrollContainer: {
+    flexGrow: 1,
     alignItems: "center",
     paddingVertical: responsive.scale(20),
     width: "100%",
-    flexGrow: 1,
   },
   posterImage: {
     width: "100%",
     height: undefined,
     aspectRatio: 2 / 3,
     marginBottom: responsive.scale(10),
-    borderRadius: 6,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
+    borderRadius: responsive.scale(6),
+    resizeMode: "contain",
   },
   subHeader: {
     fontFamily: "Arvo-Italic",
@@ -51,6 +47,7 @@ export const factsStyles = StyleSheet.create({
     textAlign: "center",
     marginBottom: responsive.scale(10),
     color: "#444",
+    lineHeight: responsive.responsiveFontSize(18),
   },
   pressable: {
     width: "100%",
