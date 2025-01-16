e577e878d3afabb1431537997defdf2d1c65742e
Tom Szymanski <szymanski.tom@gmail.com>
2025-01-16 07:45:54 -0600

    trying to remove layout file

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,26 @@
 import React from "react"
 import { Tabs } from "expo-router"
 import { colors } from "../../styles/global"
+import { useFonts } from "expo-font"
+import LoadingIndicator from "../../components/loadingIndicator"
+import ErrorMessage from "../../components/errorMessage"
 
 const TabLayout = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    "Arvo-Bold": require("../../assets/fonts/Arvo-Bold.ttf"),
+    "Arvo-Italic": require("../../assets/fonts/Arvo-Italic.ttf"),
+    "Arvo-Regular": require("../../assets/fonts/Arvo-Regular.ttf"),
+  })
+
+  if (!fontsLoaded) {
+    console.log("fonts not loaded")
+    return <LoadingIndicator />
+  }
+
+  if (fontError) {
+    return <ErrorMessage message={fontError.message} />
+  }
+
   return (
     <Tabs
       screenOptions={{
