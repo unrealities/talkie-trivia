663eb2da704d6e776d23768b2409f77907e3c361
Tom Szymanski <szymanski.tom@gmail.com>
2025-10-28 08:26:58 -0500

    removing unused files

--- src/utils/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Alert } from "react-native"
-
-export function handleError(error, context) {
-  const errorMessage =
-    error instanceof Error ? error.message : "An unknown error occurred."
-  console.error(`${context}: ${errorMessage}`)
-  Alert.alert("Error", errorMessage)
-}
